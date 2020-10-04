358ceae38e9601543293120d54195c1930a2b2b1
Mohammad Abbaspour <mhagalaxy@gmail.com>
2020-10-04 23:22:09 +0330

    build folder added

---
+++ build/precache-manifest.aa2fbe1c24ed1a522cce68dd4e8d2529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63d3ebf027cfcbefab25a35d2e1778a",
+    "url": "/20_React-Portfolio_React/index.html"
+  },
+  {
+    "revision": "09149998e0b04bde6bce",
+    "url": "/20_React-Portfolio_React/static/css/main.5bac3885.chunk.css"
+  },
+  {
+    "revision": "c1374cbc2c48100da86c",
+    "url": "/20_React-Portfolio_React/static/js/2.38f5aed1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/20_React-Portfolio_React/static/js/2.38f5aed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09149998e0b04bde6bce",
+    "url": "/20_React-Portfolio_React/static/js/main.54c2159a.chunk.js"
+  },
+  {
+    "revision": "73bab1149cda01c406c8",
+    "url": "/20_React-Portfolio_React/static/js/runtime-main.db935113.js"
+  },
+  {
+    "revision": "409ce3bb961eb5c9735a5f16fa8f779a",
+    "url": "/20_React-Portfolio_React/static/media/download.409ce3bb.jpg"
+  }
+]);
